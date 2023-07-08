c3bd591785069b6201761638797d3fcf0bcb9b57
Kimyobu <phanarin677@gmail.com>
2023-07-08 18:50:25 +0700

    Biggest Update

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-import sys
+def join(d: str, l: list[str], *a:str):
+    for x in a:
+        l.append(x)
+    return d.join(l)
+
+# l = 'sky\nkim\nyou\nbun'
+# s = l.split('\n')
+# print(join('\n', s, 'new'))
+def check():
+    import os
+    if not os.environ['setup']:
+        raise SystemExit('\033[31mRun SetUP WebUI FIRST!!\033[39m')
+
+
 import os
-
-def leave():
-    raise StopIteration()
-
-
-print('Start')
-leave()
-print('Not leave')
+if not os.environ.get('setup'):
+    raise UserWarning('\033[31mRun SetUP WebUI FIRST!!\033[39m')
+    
+print('\033[31mtest\033[39m')
